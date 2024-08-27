91ba942ed422df5c6a05d2a6fd3f7b8c78401b64
Cooper <nathan.cooper834@gmail.com>
2024-08-27 10:23:33 -0500

    disable dts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), dts({ rollupTypes: true })],
+  plugins: [react(), tsconfigPaths()],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
